cb7756bf49a3c6719f13d2c52ae823e453840105
dragon20002 <dragon20002@naver.com>
2022-02-17 13:11:41 +0900

    Update `profile load`

--- 1. profile_load.bat
+++ 1. profile_load.bat
@@ -7,7 +7,7 @@ SET    dir_git=%cur_path%\%dir_apps%\Git\bin\;
 @REM VS Code 설정
 @REM 1. VS Code 이전 설정 적용하기
 XCOPY "%cur_path%\%dir_profile%\settings.json" "C:\Users\%username%\AppData\Roaming\Code\User\"
-XCOPY "%cur_path%\%dir_profile%\.vscode\" "C:\Users\%username%\.vscode" /E/H
+XCOPY "%cur_path%\%dir_profile%\.vscode\" "C:\Users\%username%\.vscode\" /E/H
 
 @REM 2. 레지스트리 등록 (파일/폴더 VS Code로 열기)
 %cur_path%\%dir_installs%\vsCodeOpenFolder.reg
